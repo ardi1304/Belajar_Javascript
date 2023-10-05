a561e78f1d0aa6f647d63f3a4f9d6aa67ac6545e
qthinkdev <ardiandrisuhendri1304@gmail.com>
2023-10-05 15:58:23 +0700

    contoh_sarangsegitiga.js

---
+++ contoh_sarangsegitiga.js
@@ -0,0 +1,10 @@
+var segiTiga = '';
+for (var i = 5; i > 0; i--) {
+    for (var j = 0; j < i; j++) {
+        // s = s + '*';
+        segiTiga += '*';
+    } 
+    segiTiga += '\n';
+    
+}
+console.log(segiTiga);
