9b763f87d07e159424a991499e12e9acf1824996
ardi1304 <ardiandrisuhendri@gmail.com>
2023-10-07 21:59:26 +0700

    permainan suit jawa

---
+++ contoh_belahketupat.js
@@ -0,0 +1,21 @@
+//contoh belah ketupat
+var segiTiga = '';
+for (var i = 1; i <= 10; i++) {
+    for (var j = 10; j >= i; j--) {
+        segiTiga += ' ';
+    }
+    for (var k = 1; k <= i + (i-1); k++) {
+        segiTiga += '*';
+    }
+    segiTiga += '\n';
+}
+for (var i = 1; i <= 10; i++){
+    for (var j = 1; j <= i; j++) {
+        segiTiga += ' ';
+    }
+    for (var k = 10; k >= (2*i -10); k--) {
+        segiTiga += '*';
+    }
+    segiTiga += '\n';
+}
+console.log(segiTiga);
--- contoh_segitiga.js
+++ contoh_segitiga.js
@@ -1,6 +1,6 @@
 var segiTiga = '';
 for (var i = 0 ; i < 10; i++) {
-    for ( var j = 0; j < i; j++) {
+    for ( var j = 0; j <= i; j++) {
         segiTiga += '*';  
     } 
     segiTiga += '\n';
@@ -14,24 +14,5 @@ for (var i = 10 ; i > 0; i--) {
 console.log(segiTiga);
 
 
-//contoh belah ketupat
-var segiTiga = '';
-for (var i = 1; i <= 10; i++) {
-    for (var j = 10; j >= i; j--) {
-        segiTiga += ' ';
-    }
-    for (var k = 1; k <= i + (i-1); k++) {
-        segiTiga += '*';
-    }
-    segiTiga += '\n';
-}
-for (var i = 1; i <= 10; i++){
-    for (var j = 1; j <= i; j++) {
-        segiTiga += ' ';
-    }
-    for (var k = 10; k >= (2*i -10); k--) {
-        segiTiga += '*';
-    }
-    segiTiga += '\n';
-}
-console.log(segiTiga);
+
+
