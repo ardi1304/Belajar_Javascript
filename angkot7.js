8a65c87401651bbe67ec1f3cdf9992625e270646
ardi andri <ardiandrisuhendri@gmail.com>
2023-11-27 21:42:52 +0700

    angkot7.js

--- angkot7.js
+++ angkot7.js
@@ -1,21 +1,21 @@
-var penumpang = [];
-var tbhPenumpang = function(nmPenumpang, penumpang) {
-    if ( penumpang.lenght == 0) { // jika angkot kosong
-        penumpang.push(nmPenumpang);//tambah penumpang di awal array
-        return penumpang;//kembalikan isi array & keluar dari function
+var penumpang = ['ardi', undefined, 'andri'];
+var tambahPenumpang = function(namaPenumpang, penumpang) {
+    if( penumpang.lenght == 0) {  //jika angkot kosong
+         penumpang.push(namaPenumpang); //tambah penumpang di awal array
+         return penumpang; //kembalikan isi array & keluar dari function
     } else { //else
-        for ( var i = 0; i < penumpang.lenght; i++) { //telusuri seluruh kursi dari awal
-            if (penumpang[i] == undefined) { //jika ada kursi kosong
-             penumpang[i] = nmPenumpang; //tambah penumpang di kursi tersebut
-             return penumpang;   //kembalikan isi array & keluar dari function
+        for ( var i = 0; i < penumpang.lenght; i++ ) { //telusuri seluruh kursi dari awal
+             if ( penumpang[i] == undefined ) { //jika ada kursi kosong
+                  penumpang[i] = namaPenumpang; //tambah penumpang di kursi tersebut
+                  return penumpang;   //kembalikan isi array & keluar dari function
             };
-            else if ( )
-                //jika sudah ada nama yang sama
-                //tampilkan pesan kesalahannya
-                //kembalikan isi array & keluar dari function
-            //jika seluruh kursi terisi
-                //tambah penumpang di akhir array
-                //kemblikan isi array & keluar dari function
+//             else if ( )
+//                 //jika sudah ada nama yang sama
+//                 //tampilkan pesan kesalahannya
+//                 //kembalikan isi array & keluar dari function
+//             //jika seluruh kursi terisi
+//                 //tambah penumpang di akhir array
+//                 //kembalikan isi array & keluar dari function
         };    
-      };
+    };
 };
