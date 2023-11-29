c6566ba052c6f004297a8494ad60fb7658805985
ardi andri <ardiandrisuhendri@gmail.com>
2023-11-29 10:26:21 +0700

    fitur jika sudah ada penumpang di angkot7.js

--- angkot7.js
+++ angkot7.js
@@ -1,21 +1,22 @@
-var penumpang = ['ardi', undefined, 'andri'];
+var penumpang = ['Sandhika', undefined, 'Doddy'];
 var tambahPenumpang = function(namaPenumpang, penumpang) {
-    if( penumpang.lenght == 0) {  //jika angkot kosong
+    if( penumpang.length == 0) {  //jika angkot kosong
          penumpang.push(namaPenumpang); //tambah penumpang di awal array
          return penumpang; //kembalikan isi array & keluar dari function
     } else { //else
-        for ( var i = 0; i < penumpang.lenght; i++ ) { //telusuri seluruh kursi dari awal
+        for ( var i = 0; i < penumpang.length; i++ ) { //telusuri seluruh kursi dari awal
              if ( penumpang[i] == undefined ) { //jika ada kursi kosong
                   penumpang[i] = namaPenumpang; //tambah penumpang di kursi tersebut
                   return penumpang;   //kembalikan isi array & keluar dari function
-            };
-//             else if ( )
-//                 //jika sudah ada nama yang sama
-//                 //tampilkan pesan kesalahannya
-//                 //kembalikan isi array & keluar dari function
-//             //jika seluruh kursi terisi
-//                 //tambah penumpang di akhir array
-//                 //kembalikan isi array & keluar dari function
-        };    
-    };
-};
+            }
+            else if ( penumpang[i] == namaPenumpang) {    //jika sudah ada nama yang sama
+                console.log(namaPenumpang + ' sudah ada di dalam angkot.'); //tampilkan pesan kesalahannya
+                return penumpang; //kembalikan isi array & keluar dari function
+            }
+            else if( i == penumpang.length - 1) { //jika seluruh kursi terisi
+                 penumpang.push(namaPenumpang); //tambah penumpang di akhir array
+                 return penumpang; //kembalikan isi array & keluar dari function
+            }
+        }    
+    }
+}
