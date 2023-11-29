1c4b65ed0c2c40eb5fb512200382d8d5ede16f56
ardi andri <ardiandrisuhendri@gmail.com>
2023-11-29 15:25:01 +0700

    fitur hapus penumpang di angkot7.js

--- angkot7.js
+++ angkot7.js
@@ -1,4 +1,4 @@
-var penumpang = ['Sandhika', undefined, 'Doddy'];
+var penumpang = [];
 var tambahPenumpang = function(namaPenumpang, penumpang) {
     if( penumpang.length == 0) {  //jika angkot kosong
          penumpang.push(namaPenumpang); //tambah penumpang di awal array
@@ -19,4 +19,28 @@ var tambahPenumpang = function(namaPenumpang, penumpang) {
             }
         }    
     }
+}
+
+var hapusPenumpang = function(namaPenumpang, penumpang) {
+    if (penumpang.length == 0) { //jika angkot kosong
+        console.log('Angkot masih kosong.'); //tampilkan pesan bahwa angkot kosong, dan
+        //tidak mungkin ada penumpang turun    
+    } else { //else
+        for ( var i = 0; i < penumpang.length; i++ ) { //telusuri seluruh kursi dari awal
+            if (penumpang[i] == namaPenumpang) { //jika nama penumpang sesuai
+                penumpang[i] = undefined; //hapus penumpang dengan mengubah namanya, 
+                //menjadi undefined
+                return penumpang; //kembalikan isi array dan keluar dari function
+            } else if (penumpang[i] == namaPenumpang ) { //jika tidak ada nama yang sesuai 
+                console.log(namaPenumpang + ' sudah ada di dalam angkot.'); //tampilkan pesan kesalahannya
+                return penumpang; //kembalikan isi array & keluar dari function
+                
+            } else if (i == penumpang.length - 1 ){
+                console.log (namaPenumpang + ' tidak ada di dalam angkot.');//tampilkan pesan kesalahannya
+                return penumpang; //kembalikan isi array dan keluar dari function
+
+            }
+        }
+    }
+    return penumpang; //kembalikan isi array dan keluar dari function               
 }
